878bc853e60b2652bc04aabc6437db9eda56d43b
m67899932-boop <m67899932@gmail.com>
2025-11-29 22:14:42 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}', './app/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#e11a24'
+      }
+    }
+  },
+  plugins: []
+}
