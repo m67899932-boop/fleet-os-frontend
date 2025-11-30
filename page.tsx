b74a416685825ffe8bacc7a89299ebf8bb76b8e0
m67899932-boop <m67899932@gmail.com>
2025-11-29 23:46:25 -0300

    Delete page.tsx

--- page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main className="p-8">
-      <h1 className="text-primary text-3xl font-bold">Sistema OS - Frontend</h1>
-      <p className="mt-4">Frontend funcionando com sucesso.</p>
-    </main>
-  );
-}
