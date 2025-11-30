b1bdb01cf6c201bb14670e188c5bfd19bd088e43
m67899932-boop <m67899932@gmail.com>
2025-11-29 23:01:55 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: { colors: { primary: "#e11a24" } } },
-  plugins: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#e11a24"
+      }
+    }
+  },
+  plugins: []
 };
